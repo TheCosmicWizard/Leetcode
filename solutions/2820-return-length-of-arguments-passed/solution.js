7afe5eca6f8045d8343fc95e5dbb7b16cac18506
Atharv Kadam <atharv.2285@gmail.com>
2026-01-21 07:08:17 +0000

    Sync LeetCode submission Runtime - 38 ms (81.27%), Memory - 54 MB (15.83%)

---
+++ solutions/2820-return-length-of-arguments-passed/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let n = args.length;
+    return n
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
