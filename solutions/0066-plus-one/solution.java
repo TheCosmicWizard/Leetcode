bc5ca8483aaf18713fb5cd147c1ae07bdb0816f8
Atharv Kadam <atharv.2285@gmail.com>
2026-06-30 18:19:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (61.93%)

--- solutions/0066-plus-one/solution.java
+++ solutions/0066-plus-one/solution.java
@@ -2,15 +2,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for(int i = n - 1; i >= 0 ; i--){
-            if (digits[i] < 9){
-            digits[i] ++;
-            return digits;
+        for (int i = n - 1 ; i >= 0; i--){
+           if(digits[i] < 9){
+                digits[i]=1+digits[i];
+                return digits; 
             }
-            digits[i]=0;
+            if(digits[i] == 9){
+                if(i == 0){
+                int[] arr = new int[n+1];
+                arr[0] = 1;
+                return arr;
+            }
+                digits[i] = 0;
+            }
+            
         }
-        int[] arr = new int[n+1];
-        arr[0]=1;
-        return arr;
+        return digits;
     }
 }
