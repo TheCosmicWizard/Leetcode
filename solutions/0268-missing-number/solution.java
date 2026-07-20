9dec03ac16c79a14f8c4f25de13b5ba847c1acad
Atharv Kadam <atharv.2285@gmail.com>
2026-07-20 11:50:17 +0000

    Sync LeetCode submission Runtime - 1 ms (29.65%), Memory - 47.2 MB (66.35%)

---
+++ solutions/0268-missing-number/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i];
+            if(i != nums[i] & correct < nums.length){
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j = 0; j < nums.length;j++){
+            if (nums[j] != j){
+                return j;
+            }
+        }
+        return nums.length;
+    }
+}
