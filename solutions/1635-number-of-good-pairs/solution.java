dd711131f6a5f7a2e895d95481d50f6bff67d276
Atharv Kadam <atharv.2285@gmail.com>
2026-08-06 18:11:18 +0000

    Sync LeetCode submission Runtime - 1 ms (85.90%), Memory - 42.6 MB (90.31%)

--- solutions/1635-number-of-good-pairs/solution.java
+++ solutions/1635-number-of-good-pairs/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int count = 0;
+        int value = 0;
         for(int i = 0;i< nums.length;i++ ){
             for(int j=i+1; j <nums.length;j++){
                 if(nums[i]==nums[j] && i!=j){
-                   count++; 
+                   value++; 
                 }
             }
         }
-        return count;
+        return value;
     }
 }
