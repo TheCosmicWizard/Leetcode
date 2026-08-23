206476a77ecf598c5bb418543baf0be01f21d858
Atharv Kadam <atharv.2285@gmail.com>
2026-08-23 18:45:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (15.56%)

---
+++ solutions/0342-power-of-four/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n < 1){
+            return false;
+        }
+        if(n == 1){
+            return true;
+        }else if(n % 4 == 0){
+            return isPowerOfFour(n/4);
+        }else{
+            return false;
+        }
+    }
+}
